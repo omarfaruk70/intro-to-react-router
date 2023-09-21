39cf78f27156c89c888633d797bbf91e79a905ef
omarfaruk70 <faruk1611281892@gmail.com>
2023-09-22 01:52:13 +0600

    added useNavigate hook to navigate any route or go back

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,7 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const User = ({user}) => {
     const {id, name, phone, email} = user;
+    const navigate = useNavigate(); // to navigate we use this hook
+    const gobackOneStep = ()=>{
+        navigate(-1);
+        // navigate('user/${user.id}') we can also use like it
+    }
     const userStyle = {
         border: '5px solid pink',
         margin: '10px',
@@ -14,6 +19,8 @@ const User = ({user}) => {
             <p>Email: {email}</p>
             <p>Phone: {phone}</p>
             <Link to={`user/${user.id}`}>Show Details</Link>
+            {/* go back one step with useNavigate hook */}
+            <button onClick={gobackOneStep}>Go back</button>
         </div>
     );
 };
